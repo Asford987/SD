7fb60cda043cafeafca407f15ee2afe640d0d66e
Fabricio Assunção <fabricio.asfora.001@gmail.com>
2025-05-24 19:27:18 -0300

    Update mpi_saco.c

--- src/mpi_saco.c
+++ src/mpi_saco.c
@@ -4,31 +4,6 @@
 #include <stdint.h>
 
 
-
-float Q_rsqrt(float number) {
-    long i;
-    float x2, y;
-    const float threehalfs = 1.5F;
-
-    x2 = number * 0.5F;
-    y  = number;
-    
-    // Evil floating point bit level hacking
-    i  = *(long *)&y;                     // treat float's bits as integer
-    i  = 0x5f3759df - (i >> 1);           // initial guess
-    y  = *(float *)&i;
-
-    // One iteration of Newton-Raphson
-    y  = y * (threehalfs - (x2 * y * y)); // improve estimate
-
-    return y;
-}
-
-float sqrt(float number) {
-    return 1.0f / Q_rsqrt(number);
-}
-
-
 #define TAMANHO_TAREFA 500000
 #define TAG_TAREFA 1
 #define TAG_RESULTADO 2
@@ -40,7 +15,7 @@ int primo(long int n) {
     if (n % 2 == 0) return 0;
 
     long int i;
-    for (i = 3; i <= sqrt(n); i += 2) {
+    for (i = 3; i*i <= n; i += 2) {
         if (n % i == 0)
             return 0;
     }
